b0d93d4be14ca50ce21c1bc3d957558bf2ca63f5
Farhan Sabbir Siddique <fsabbir@gmail.com>
2021-05-09 01:28:59 +0600

    Standalone and service containers trapped
    Both start and dying standalone containers have equal info except for CMD for register and deregister.
    standalone containers have service tagging naming pulled from container name
    now will need to add port mapping and labels

--- docker_event_capture.py
+++ docker_event_capture.py
@@ -1,3 +1,62 @@
+#!/bin/env python3
+import os
+import sys
+import json
 import docker
 import requests
+import datetime
 
+DOCKER_CLIENT = None
+CONFIG = None
+
+def init():
+    global DOCKER_CLIENT
+    try:
+        CONFIG = json.load(open(sys.argv[1]))
+        DOCKER_CLIENT = docker.DockerClient(base_url='unix:/' + str(CONFIG["docker"]))
+        for container in (DOCKER_CLIENT.containers.list(filters={"status":"running"})):
+            print(container.attrs["Id"])
+        
+        event_loop()
+
+    except docker.errors.DockerException as dockererror:
+        print("Unable to connect to docker daemon. Reason: " + str(dockererror))
+        exit(1)
+    except KeyError as err:
+        print("Config parsing error. Key not found in config: " + str(err))
+        exit(1)
+
+def event_loop():
+    for event in DOCKER_CLIENT.events(decode=True):
+        if (event["Type"]=="container"):
+            if event["status"] == "start" or event["status"] == "destroy":
+                PAYLOAD = dict()
+                PAYLOAD["CONTAINER_ID"] = event["id"]
+                if "Attributes" in event["Actor"]:
+                    ATTRS = event["Actor"]["Attributes"]
+                    PAYLOAD["SVC_NAME"] = ATTRS["name"]
+                    PAYLOAD["SERVICE"] = False
+                    if "com.docker.swarm.service.id" in ATTRS:
+                        PAYLOAD["SERVICE"] = True
+                        PAYLOAD["SVC_ID"] = ATTRS["com.docker.swarm.service.id"]
+                        PAYLOAD["SVC_NAME"] = ATTRS["com.docker.swarm.service.name"]
+                    PAYLOAD["CONTAINER_NAME"] = ATTRS["name"]
+                    PAYLOAD["IMAGE_NAME"] = ATTRS["image"]
+                if event["status"] == "start":
+                    PAYLOAD["CMD"] = "register"
+                    print(json.dumps(PAYLOAD))
+                    #print(json.dumps(fetch_container_details(PAYLOAD["CONTAINER_ID"]).attrs))
+                else:
+                    PAYLOAD["CMD"] = "deregister"
+                    notify_consul(PAYLOAD)
+
+def fetch_container_details(id):
+    return DOCKER_CLIENT.containers.get(id)
+
+def notify_consul(payload):
+    print(json.dumps(payload))
+
+
+
+if __name__ == "__main__":
+    init()
