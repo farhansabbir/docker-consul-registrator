639fe1d5bd70599c539abe242d6d2cf670323973
Farhan Sabbir Siddique <fsabbir@gmail.com>
2021-05-21 01:30:09 +0600

    Adds syslog facility for generic logging to OS syslog

--- registrator.py
+++ registrator.py
@@ -3,6 +3,7 @@ try:
     import sys
     import json
     import docker
+    import syslog
     import requests
     import platform
 except ModuleNotFoundError as err:
@@ -53,7 +54,7 @@ def generate_Payload_For_Registration(container=None):
         payload["Connect"] = {"SidecarService":{}}
     payload["ID"] = container.id
     if is_A_Service_Container(container=container):
-        # print("Service container: " + str(container.id))
+        # syslog.syslog(syslog.LOG_INFO,"Service container: " + str(container.id))
         svc = get_Service_Container_Details(container=container).attrs
         payload["Name"] = svc["Spec"]["Name"]
         payload["Meta"] = svc["Spec"]["Labels"]
@@ -61,12 +62,12 @@ def generate_Payload_For_Registration(container=None):
         if CONFIG["consul_registration_label"] not in payload["Meta"]:
             return None
         if len(svc["Endpoint"]["Ports"]) > 1:
-            print("WARNING! Multiple port bindings found. Using single exposed port only. This may impact service registration.")
+            syslog.syslog(syslog.LOG_INFO,"WARNING! Multiple port bindings found. Using single exposed port only. This may impact service registration.")
         payload["Check"][svc["Endpoint"]["Ports"][0]["Protocol"]] = CONFIG["self_ip"] + ":" + str(svc["Endpoint"]["Ports"][0]["PublishedPort"])
         payload["Address"] = CONFIG["self_ip"]
         payload["Port"] = int(svc["Endpoint"]["Ports"][0]["PublishedPort"])
     else:
-        # print("Regular container: " + str(container.id))
+        # syslog.syslog(syslog.LOG_INFO,"Regular container: " + str(container.id))
         payload["Name"] = container.name
         payload["Meta"] = container.attrs["Config"]["Labels"]
         payload["Tags"].append(str(payload["Meta"]))
@@ -75,7 +76,7 @@ def generate_Payload_For_Registration(container=None):
             return None
         for portproto,mapping in container.attrs["HostConfig"]["PortBindings"].items():
             if len(mapping) > 1:
-                print("Warning! Multiple ports exposed. Will select only first one: " + str(mapping[0]))
+                syslog.syslog(syslog.LOG_INFO,"Warning! Multiple ports exposed. Will select only first one: " + str(mapping[0]))
             IP = CONFIG["self_ip"]
             if mapping[0]["HostIp"] != "":
                 IP = mapping[0]["HostIp"]
@@ -90,24 +91,24 @@ def deregister_Service_From_Consul(id=None):
     headers = {"Content-type": "application/json"}
     resp = requests.put(CONFIG["consul"] + "/v1/agent/service/deregister/" + id, headers=headers)
     if resp.status_code == 200:
-        print("Successfully deregistered container " + id +  " from consul.")
+        syslog.syslog(syslog.LOG_INFO,"Successfully deregistered container " + id +  " from consul.")
     else:
-        print("Unable to deregister container " + id +  " from consul because " + str(resp.text))
+        syslog.syslog(syslog.LOG_INFO,"Unable to deregister container " + id +  " from consul because " + str(resp.text))
 
 
 
 def register_Service_To_Consul(container=None):
     data = generate_Payload_For_Registration(container=container)
     if not data:
-        print("Container '" + str(container.name) + " (" + str(container.id) + ")' is not labelled to register with consul. Skipping.")
+        syslog.syslog(syslog.LOG_INFO,"Container '" + str(container.name) + " (" + str(container.id) + ")' is not labelled to register with consul. Skipping.")
         return None
     headers = {"Content-type": "application/json"}
     resp = requests.put(CONFIG["consul"] + "/v1/agent/service/register",json=data, headers=headers)
     if resp.status_code == 200:
-        print("Successfully registered container (" + str(container.name) + " (" + str(container.id) + ")' to consul.")
+        syslog.syslog(syslog.LOG_INFO,"Successfully registered container (" + str(container.name) + " (" + str(container.id) + ")' to consul.")
     else:
-        print("Unable to register container '" + str(container.name) + " (" + str(container.id) + ")' to consul.")
-        print(resp.text)
+        syslog.syslog(syslog.LOG_INFO,"Unable to register container '" + str(container.name) + " (" + str(container.id) + ")' to consul.")
+        syslog.syslog(syslog.LOG_INFO,resp.text)
 
 
 
@@ -145,40 +146,40 @@ def get_Container_List_From_Host(status="running"):
 
 
 def cleanup():
-    print("Running cleanup.")
-    print("Validating with running containers first.")
+    syslog.syslog(syslog.LOG_INFO,"Running cleanup.")
+    syslog.syslog(syslog.LOG_INFO,"Validating with running containers first.")
     for container in DOCKER_CLIENT.containers.list():
         if not is_Container_Registered_To_Consul(container=container):
             register_Service_To_Consul(container)
         else:
-            print("Container '" + str(container.name) + " (" + str(container.id) + ")' is already registered to consul. Skipping.")
-    print("Cleaning up old stale/exited containers. Found total: " + str(len(get_Container_List_From_Host(status="exited"))))
+            syslog.syslog(syslog.LOG_INFO,"Container '" + str(container.name) + " (" + str(container.id) + ")' is already registered to consul. Skipping.")
+    syslog.syslog(syslog.LOG_INFO,"Cleaning up old stale/exited containers. Found total: " + str(len(get_Container_List_From_Host(status="exited"))))
     for container in get_Container_List_From_Host(status="exited"):
         if is_Container_Registered_To_Consul(container=container):
-            print("Container is still registered with consul. Deregistering.")
+            syslog.syslog(syslog.LOG_INFO,"Container is still registered with consul. Deregistering.")
             deregister_Service_From_Consul(container.id)
         else:
-            print("Please remove old container " + container.id + " from system.")
+            syslog.syslog(syslog.LOG_INFO,"Please remove old container " + container.id + " from system.")
             # container.remove()
-    print("Cleaning up for registered containers from consul that don't exist in system anymore.")
+    syslog.syslog(syslog.LOG_INFO,"Cleaning up for registered containers from consul that don't exist in system anymore.")
 
     for svc_id in (get_Registered_Services_From_Consul()).keys():
         if svc_id in ["ingress-service","sidecar"]:
             continue
         if (svc_id not in [container.id for container in get_Container_List_From_Host()]):
-            print("Deregistering non-existant container '" + svc_id + "' from consul")
+            syslog.syslog(syslog.LOG_INFO,"Deregistering non-existant container '" + svc_id + "' from consul")
             deregister_Service_From_Consul(svc_id)
 
-    print("Initial cleanup complete.")
+    syslog.syslog(syslog.LOG_INFO,"Initial cleanup complete.")
         
 
 def check_consul_connection():
     try:
         if requests.get(CONFIG["consul"],timeout=2).status_code != 200:
-            print("Unable to contact consul service on " + str(CONFIG["consul"]))
+            syslog.syslog(syslog.LOG_INFO,"Unable to contact consul service on " + str(CONFIG["consul"]))
             exit(1)
     except requests.exceptions.ConnectionError as err:
-        print("Unable to contact consul service on " + str(CONFIG["consul"]) + ".\n" + str(err))
+        syslog.syslog(syslog.LOG_INFO,"Unable to contact consul service on " + str(CONFIG["consul"]) + ".\n" + str(err))
         exit(1)
 
 
@@ -207,14 +208,14 @@ def init():
         check_consul_connection()
 
     except IndexError:
-        print("You need to provide a configuration file (config.json, typically) as an argument with this script. ")
+        syslog.syslog(syslog.LOG_INFO,"You need to provide a configuration file (config.json, typically) as an argument with this script. ")
         exit(1)
 
     except docker.errors.DockerException as dockererror:
-        print("Unable to connect to docker daemon. Reason: " + str(dockererror))
+        syslog.syslog(syslog.LOG_INFO,"Unable to connect to docker daemon. Reason: " + str(dockererror))
         exit(1)
     except KeyError as err:
-        print("Config parsing error. Key not found in config: " + str(err))
+        syslog.syslog(syslog.LOG_INFO,"Config parsing error. Key not found in config: " + str(err))
         exit(1)
 
     cleanup()
